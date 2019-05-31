1746c6e1e574691e49f33bdca1a27f91812be500
Ingrid Fuentes <ingridfmelgar@gmail.com>
2019-05-31 05:21:14 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,29 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)  # code an argument here
+    dwarves = ["Dopey", "Grumpy", "Bashful", "Sneezy", "Sleepy" "Hungry", "Doc"]
+      dwarves.each_with_index {|dwarf, index| hash[dwarf] = index puts /#{index}.*#{dwarf}/ }
+    end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(veggies)  # code an argument here
+     veggies = ["carrot", "cucumber", "pepper"]
+     veggies.map! {|veggie| veggie.capitalize}
+     veggies.each {|veggie| puts veggie + "!"}
+     veggies.class 
+     veggies.length 
+   end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls_long_length) # code an argument here
+      calls_long = ["axe", "earth", "wind", "fire"]
+        calls_long.length >= 4
+      end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(contains_cheddar)   # code an argument here
+    #the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  contains_cheddar = ["banana", "cheddar", "sock"]
+  contains_cheddar[1]
+  no_cheese.include?('cheese')  #nil
+  end
 end
